97fffc518dcb6e7de065dff9b0c3f8880bd54e35
GuilhermeReis120 <souzaguireis@gmail.com>
2025-10-12 21:53:57 -0300

    atualização 2.0

--- public/dashboard.php
+++ public/dashboard.php
@@ -7,8 +7,7 @@ require_once '../src/Templates/header.php';
 ?>
 
 <div class="content-box">
-  <h1>Página Protegida</h1>
-  <p>Bem-vindo à tela de Dashboard, <?= htmlspecialchars($_SESSION['usuario_nome']); ?>!</p>
+  <h1>Bem-vindo à tela de Dashboard, <?= htmlspecialchars($_SESSION['usuario_nome']); ?>!</h1>
 </div>
 
 <?php
